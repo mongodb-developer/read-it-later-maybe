04e9de5e965dec59b741619a275a4acdebb5aeb1
Diego Freniche <diego@freniche.com>
2021-12-21 10:38:22 +0100

    Added react-native-get-random-values to silence warning "BSON: For React Native please polyfill crypto.getRandomValues"

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { registerRootComponent } from 'expo';
 
+// added to avoid ugly Warning "BSON: For React Native please polyfill crypto.getRandomValues" 
+// See: https://github.com/realm/realm-js/issues/3714
+import 'react-native-get-random-values'
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
